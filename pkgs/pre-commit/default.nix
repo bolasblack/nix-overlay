2393e6aa1cbece248e5afe35a4ef0027593448dc
c4605 <bolasblack@gmail.com>
2021-05-26 09:26:23 +0800

    pre-commit: 2.2.0 -> 2.13.0

--- pkgs/pre-commit/default.nix
+++ pkgs/pre-commit/default.nix
@@ -4,26 +4,11 @@ with pkgs;
 with pkgs.python38Packages;
 
 pre-commit.overridePythonAttrs(oldAttrs: rec {
-  version = "2.2.0";
+  version = "2.13.0";
   
   src = fetchPypi {
     inherit version;
     pname = "pre_commit";
-    sha256 = "1qb937lda4pam4q5l86i9bvdb9419vgsw8s7ak2lcysaw2y13an0";
+    sha256 = "0y53hp1vwrx78a3ikxqh2d848cgcajbb4vp8m25ndp4k0v374jbn";
   };
-
-  propagatedBuildInputs = [
-    cfgv
-    identify
-    nodeenv
-    pyyaml
-    toml
-    virtualenv
-  ] ++ lib.optional (pythonOlder "3.8") importlib-metadata
-    ++ lib.optional (pythonOlder "3.7") importlib-resources
-    ++ lib.optional (pythonOlder "3.2") futures;
-
-  patches = [
-    ./hook-tmpl-use-the-hardcoded-path-to-pre-commit.patch
-  ];
 })
